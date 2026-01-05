d6134447f1ad094fddaa3998603d24f7a34488a5
Roy Corneloues <roy@corneloues.co.uk>
2026-01-05 14:49:39 +0000

    Add script to bump patch version in version.txt

---
+++ scripts/bump-patch.ps1
@@ -0,0 +1,5 @@
+$version = Get-Content version.txt
+$parts = $version -split '\.'
+$parts[2] = [int]$parts[2] + 1
+$newVersion = "$($parts[0]).$($parts[1]).$($parts[2])"
+Set-Content version.txt $newVersion
