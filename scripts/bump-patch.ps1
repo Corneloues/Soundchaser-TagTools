6ce0b6b545602ac88ca3eff315072aacf6df64a0
Roy Corneloues <roy@corneloues.co.uk>
2026-01-05 14:52:11 +0000

    Increment patch version in bump-patch.ps1

--- scripts/bump-patch.ps1
+++ scripts/bump-patch.ps1
@@ -1,5 +1,5 @@
 $version = Get-Content version.txt
 $parts = $version -split '\.'
-$parts[2] = [int]$parts[2] + 1
+$parts[2] = [int]$parts[2] + 1                              // Bump patch
 $newVersion = "$($parts[0]).$($parts[1]).$($parts[2])"
 Set-Content version.txt $newVersion
