8a98f5bff53dd67df81ac928b1a145623625d360
Roy Corneloues <roy@corneloues.co.uk>
2026-01-05 15:05:23 +0000

    Update bump-minor.ps1

--- scripts/bump-minor.ps1
+++ scripts/bump-minor.ps1
@@ -1,5 +1,5 @@
 $version = Get-Content version.txt
 $parts = $version -split '\.'
 $parts[1] = [int]$parts[1] + 1                              // Bump minor
-$newVersion = "$($parts[0]).$($parts[1]).$($parts[2])"
+$newVersion = "$($parts[0]).$($parts[1]).0)"
 Set-Content version.txt $newVersion
