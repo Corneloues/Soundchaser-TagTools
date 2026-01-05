2ebda31732a2a137510f31c37056c5ac424a881c
Roy Corneloues <roy@corneloues.co.uk>
2026-01-05 15:13:07 +0000

    Fix comment syntax in bump-major.ps1

--- scripts/bump-major.ps1
+++ scripts/bump-major.ps1
@@ -1,5 +1,5 @@
 $version = Get-Content version.txt
 $parts = $version -split '\.'
-$parts[0] = [int]$parts[0] + 1                              // Bump major
+$parts[0] = [int]$parts[0] + 1                              # Bump major
 $newVersion = "$($parts[0]).0.0)"
 Set-Content version.txt $newVersion
