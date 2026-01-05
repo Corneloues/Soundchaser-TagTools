5e96164d452e42870b21d7878a490c8f0c3fd3bd
Roy Corneloues <roy@corneloues.co.uk>
2026-01-05 15:16:38 +0000

    Bump minor version and reset patch version

--- scripts/bump-minor.ps1
+++ scripts/bump-minor.ps1
@@ -1,5 +1,6 @@
 $version = Get-Content version.txt
 $parts = $version -split '\.'
 $parts[1] = [int]$parts[1] + 1                              # Bump minor
-$newVersion = "$($parts[0]).$($parts[1]).0"
+$parts[2] = 0                                               # Reset patch
+$newVersion = "$($parts[0]).$($parts[1]).$($parts[2])"
 Set-Content version.txt $newVersion
